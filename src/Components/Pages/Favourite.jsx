992b1c910ee50ff002cf16cb714740b460fefe70
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-17 16:58:56 +0100

    .env configuration and pursuit works on routes creation and links

--- src/Components/Pages/Favourite.jsx
+++ src/Components/Pages/Favourite.jsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 import './Favourite.css';
 
 export default function Favourite() {
   return (
     <>
+      <Link to='/mon-favori'>Favori sélectionné</Link>
       <p>Page de recherche des favoris</p>
     </>
   );
