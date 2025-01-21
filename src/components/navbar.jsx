234998cca6347148f9ef7a8957e055a470131fc0
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-21 22:08:33 +0100

    navBar.jsx is interactive, icon is active if we are on the correct page and inactive if not

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { iconList } from './iconList';
 import './navbar.scss';
 
-export default function Navbar() {
+export default function Navbar(props) {
+  //console.log('mon chemin : ' + window.location.href);
+
+  const [isActive, setIsActive] = useState(props.isActive);
+
+  function handleClickPage() {
+    setIsActive(() => window.location.href);
+  }
+
   return (
     <>
       {/* Création de la navbar icone + texte */}
       <nav className='NavBar'>
         {iconList.map((icone) => (
-          <Link key={icone} to={icone.link}>
-            <img src={icone.imgSrc} width={25} />
+          <Link key={icone} to={icone.link} onClick={handleClickPage}>
+            {/* condition pour afficher icone active ou inactive selon la page affichée */}
+
+            {window.location.pathname.endsWith(icone.link) ? (
+              <img src={icone.imgSrcOn} alt={icone.name} title={icone.name} />
+            ) : (
+              <img src={icone.imgSrcOff} alt={icone.name} title={icone.name} />
+            )}
+
             <p>{icone.name}</p>
           </Link>
         ))}
