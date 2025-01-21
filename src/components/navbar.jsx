da9d1845a58d5813b9e30418c6f8b2c89e36623a
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-21 17:00:31 +0100

    update navbar.jsx and to use the tabl iconList

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,30 +1,22 @@
 import { Link } from 'react-router-dom';
 
-import './navbar.scss';
 import { iconList } from './iconList';
+import './navbar.scss';
 
 export default function Navbar() {
   return (
     <>
-{/* Création de la navbar icone + texte */}
-      <nav className="NavBar">
-          {iconList.map((icone) => (
-
-              <Link key={icone} to={icone.link}>
-
-                <img src={icone.imgSrc} width={30}/>
-                {icone.name}
-
-              </Link>
-
-          ))}
+      {/* Création de la navbar icone + texte */}
+      <nav className='NavBar'>
+        {iconList.map((icone) => (
+          <Link key={icone} to={icone.link}>
+            <img src={icone.imgSrc} width={30} />
+            {icone.name}
+          </Link>
+        ))}
       </nav>
 
-
-{/* Ajouter la logique de l'insertion de la modale de "à propos de nous." */}
-
-
+      {/* Ajouter la logique de l'insertion de la modale de "à propos de nous." */}
     </>
   );
 }
-
