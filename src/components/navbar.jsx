dbbbc1c606966b96fb9a48720b2afea13cfa3d43
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-22 10:49:46 +0100

    fixed navbar  to have the correct icon depending where we are on the app

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import { iconList } from './iconList';
 import './navbar.scss';
 
-export default function Navbar(props) {
-  //console.log('mon chemin : ' + window.location.href);
+export default function Navbar() {
+  const [namePath, setNamePath] = useState('');
+  const location = useLocation(); // récupère le chemin actuel, pas besoin de déclenchement au clic
 
-  const [isActive, setIsActive] = useState(props.isActive);
-
-  function handleClickPage() {
-    setIsActive(() => window.location.href);
-  }
+  // Mise à jour de namePath dès qu'on change de page, pathname affiche exemple : "/favoris"
+  useEffect(() => {
+    setNamePath(location.pathname);
+  }, [location]);
 
   return (
     <>
       {/* Création de la navbar icone + texte */}
       <nav className='NavBar'>
         {iconList.map((icone) => (
-          <Link key={icone} to={icone.link} onClick={handleClickPage}>
-            {/* condition pour afficher icone active ou inactive selon la page affichée */}
-
-            {window.location.pathname.endsWith(icone.link) ? (
+          <Link key={icone.name} to={icone.link}>
+            {/* Affiche l'icône active ou inactive, relou car le "/" est dans tous les chemins du coup faut faire une double condi*/}
+            {/* !!!Attention les pages recherches aleatoire et erreur n'ont pas "/recherche" dans leur chemin, il faurdrait faire cette modif */}
+            {namePath === icone.link || (icone.link !== '/' && namePath.includes(icone.link)) ? (
               <img src={icone.imgSrcOn} alt={icone.name} title={icone.name} />
             ) : (
               <img src={icone.imgSrcOff} alt={icone.name} title={icone.name} />
             )}
-
+            
             <p>{icone.name}</p>
           </Link>
         ))}
