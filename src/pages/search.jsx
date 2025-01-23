d56243e3822d7f2d1c0ae1fc91e8270fd2dc92b9
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-23 15:16:49 +0100

    fixe police in index.html

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -5,9 +5,10 @@ import './search.scss';
 export default function Search() {
   return (
     <>
-      <p>Je suis la page de recherche</p>
+      <p> Je suis la page de recherche</p>
       <Link to='resultats-de-recherche'>Résultats de recherche</Link>
       <Link to='oops'>Erreur de recherche</Link>
+
       <Link to='article-aleatoire'>Lien vers les articles Aléatoires</Link>
     </>
   );
