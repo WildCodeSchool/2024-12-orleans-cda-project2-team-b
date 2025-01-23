fa6fcd8c035ba70cec003597cba47f070256399f
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-23 14:21:22 +0100

    finish navbar

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -6,9 +6,9 @@ export default function Search() {
   return (
     <>
       <p>Je suis la page de recherche</p>
-      <Link to='/resultats-de-recherche'>Résultats de recherche</Link>
-      <Link to='/oops'>Erreur de recherche</Link>
-      <Link to='/article-aleatoire'>Lien vers les articles Aléatoires</Link>
+      <Link to='resultats-de-recherche'>Résultats de recherche</Link>
+      <Link to='oops'>Erreur de recherche</Link>
+      <Link to='article-aleatoire'>Lien vers les articles Aléatoires</Link>
     </>
   );
 }
