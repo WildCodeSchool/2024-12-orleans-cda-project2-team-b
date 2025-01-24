6b989bdc0097a6468b726db9bd14b67ab8176a92
alesbesdria <melissa@meirsman.fr>
2025-01-24 15:41:59 +0100

    fix reviews

--- src/components/principal-title.jsx
+++ src/components/principal-title.jsx
@@ -3,9 +3,9 @@ import './principal-title.scss';
 export default function PrincipalTitle() {
   return (
     <>
-      <div className='principal-title-position'>
-        <h1 className='principaltitleh1'>{"Wild's News"}</h1>
-        <h2 className='principaltitleh2'>{"L'actu tech"}</h2>
+      <div className='PrincipalTitlePosition'>
+        <h1 className='PrincipalTitleH1'>{"Wild's News"}</h1>
+        <h2 className='PrincipalTitleH2'>{"L'actu tech"}</h2>
       </div>
     </>
   );
