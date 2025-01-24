8d7c6c866ad5638c2073312ba89b86affab0336d
alesbesdria <melissa@meirsman.fr>
2025-01-24 11:43:12 +0100

    add media queries on principaltitle

--- src/app.jsx
+++ src/app.jsx
@@ -2,16 +2,27 @@ import { Outlet } from 'react-router-dom';
 
 import './app.scss';
 import Navbar from './components/navbar';
+import PrincipalTitle from './components/principaltitle';
 
 function App() {
   return (
-    <div className='absolute-center-container'>
-      <nav>
-        <Navbar />
-      </nav>
-      <main>
-        <Outlet />
-      </main>
+    <div>
+      <header>
+        <div className='principal-title-mobile'>
+          <PrincipalTitle />
+        </div>
+      </header>
+      <div className='absolute-center-container'>
+        <div className='principal-title-desktop'>
+          <PrincipalTitle />
+        </div>
+        <nav>
+          <Navbar />
+        </nav>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
