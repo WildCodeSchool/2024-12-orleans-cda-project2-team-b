778644c99a6ba664bae734cebef5f28805400e68
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-27 10:30:39 +0100

    add the footer and map background

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
 import './app.scss';
+import BackgroundCard from './components/back-gard';
 import Footer from './components/footer';
 import MainTitle from './components/main-title';
 import Navbar from './components/navbar';
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <BackgroundCard />
       <MainTitle />
       <main className='absolute-center-container'>
         <Outlet />
---
+++ src/components/back-gard.jsx
@@ -0,0 +1,5 @@
+import './back-gard.scss';
+
+export default function BackgroundCard() {
+  return <img className='backgroundCard' src='/back-card.svg' />;
+}
