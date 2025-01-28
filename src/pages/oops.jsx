4448a3418137006f4e44457276f8375747ce08bf
alesbesdria <melissa@meirsman.fr>
2025-01-28 16:26:47 +0100

    fix searcherror

--- src/pages/oops.jsx
+++ src/pages/oops.jsx
@@ -5,14 +5,14 @@ import './oops.scss';
 export default function Oops() {
   return (
     <>
-      <div className='nosearchcontainer'>
+      <div className='no-search-container'>
         <p>
           Oops... 🫢
           <br />
           Nous n’avons pas de résultat pour votre demande
         </p>
         <Link to='/recherche'>
-          <img className='nosearchicon' src='/icons/search-inactive-black.png' alt='loupe lien vers recherche' />
+          <img className='no-search-icon' src='/icons/search-inactive-black.png' alt='loupe lien vers recherche' />
         </Link>
       </div>
     </>
