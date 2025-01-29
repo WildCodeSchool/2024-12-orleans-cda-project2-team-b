cc43bbe00c48a8aeeacd163bc11253a0d291d55d
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-29 13:49:55 +0100

    add function handlclick

--- src/components/pop-up-question.jsx
+++ src/components/pop-up-question.jsx
@@ -2,15 +2,21 @@ import './pop-up-question.scss';
 
 export function PopUpQuestion() {
   return (
-    <section className='pop-up-question'>
-      <h1>VOTRE EXPÉRIENCE, VOS CHOIX</h1>
-      <p>
-        Afin d’optimiser votre expérience, consentez-vous à l’enregistrement de vos articles favoris et de votre
-        historique de consultation sur notre site afin de vous permettre de les retrouver plus facilement lors d’une
-        prochaine visite ?
-      </p>
-      <button className='button-yes'>Oui</button>
-      <button className='button-no'>Non</button>
-    </section>
+    <div className='pop-up-background'>
+      <div className='pop-up-question'>
+        <h1>VOTRE EXPÉRIENCE, VOS CHOIX</h1>
+        <p>
+          Afin d’optimiser votre expérience, consentez-vous à l’enregistrement de vos articles favoris et de votre
+          historique de consultation sur notre site afin de vous permettre de les retrouver lors de votre prochaine
+          visite ?
+        </p>
+        <button className='button-yes' type='button'>
+          Oui
+        </button>
+        <button className='button-no' type='button'>
+          Non
+        </button>
+      </div>
+    </div>
   );
 }
