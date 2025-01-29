5c018943f54c9f2ad1d0c18c6df21b23fd94685a
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-29 10:23:44 +0100

    police mobile changed + fix search bar

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -13,10 +13,6 @@ const placeHolderList = [
 export default function Search() {
   return (
     <>
-      <p>Je suis la page de recherche</p>
-      <p>barre de recherche ici</p>
-      <br></br>
-
       {/*Plus tard afficher le place holder selon langue choisie */}
       <div className='searchBar'>
         <img src='/icons/search.png' />
@@ -24,9 +20,9 @@ export default function Search() {
       </div>
       <br></br>
       <br></br>
-      <Link to='/resultats-de-recherche'>Résultats de recherche</Link>
-      <Link to='/oops'>Erreur de recherche</Link>
-      <Link to='/article-aleatoire'>Lien vers les articles Aléatoires</Link>
+      <Link to='resultats-de-recherche'>Résultats de recherche</Link>
+      <Link to='oops'>Erreur de recherche</Link>
+      <Link to='article-aleatoire'>Lien vers les articles Aléatoires</Link>
       <Outlet />
     </>
   );
