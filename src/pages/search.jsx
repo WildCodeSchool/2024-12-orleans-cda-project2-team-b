d74447c3fe48d41bb543a21c69fe8b5805cc5237
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-29 12:07:04 +0100

    fixe searchbar after Sylvain's comments

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -2,24 +2,23 @@ import { Link, Outlet } from 'react-router-dom';
 
 import './search.scss';
 
-const placeHolderList = [
-  { language: 'Français', placeHolderTraduc: 'Ecrivez votre recherche en français' },
-  { language: 'English', placeHolderTraduc: 'Write your search in english' },
-  { language: 'Deutsch', placeHolderTraduc: 'Schreiben Sie Ihre Suche auf Deutsch' },
-  { language: '中文', placeHolderTraduc: '用中文写下您的搜索' },
-  { language: 'Español', placeHolderTraduc: 'Escriba su búsqueda en español' },
+const placeholderList = [
+  { language: 'Français', placeholderTraduc: 'Ecrivez votre recherche en français' },
+  { language: 'English', placeholderTraduc: 'Write your search in english' },
+  { language: 'Deutsch', placeholderTraduc: 'Schreiben Sie Ihre Suche auf Deutsch' },
+  { language: '中文', placeholderTraduc: '用中文写下您的搜索' },
+  { language: 'Español', placeholderTraduc: 'Escriba su búsqueda en español' },
 ];
 
 export default function Search() {
   return (
     <>
       {/*Plus tard afficher le place holder selon langue choisie */}
-      <div className='searchBar'>
+      <div className='search-bar'>
         <img src='/icons/search.png' />
         <input type='search' placeholder='Ecrivez votre recherche en français '></input>
       </div>
-      <br></br>
-      <br></br>
+
       <Link to='resultats-de-recherche'>Résultats de recherche</Link>
       <Link to='oops'>Erreur de recherche</Link>
       <Link to='article-aleatoire'>Lien vers les articles Aléatoires</Link>
