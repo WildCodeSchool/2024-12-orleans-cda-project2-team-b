3ed0170a737da74df8ea2e578e3833d3b2edd6a9
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-30 17:01:28 +0100

    answer of the pop up saved in local

--- src/components/pop-up-question.jsx
+++ src/components/pop-up-question.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './pop-up-question.scss';
 
+//Si l'utilisateur répond oui on garde en mémoire sa réponse en local, donc prochaine ouverture ou F5 la pop-up ne se réaffiche pas
+//Si il clique sur non alors au prochain F5 ou connexion pop-up réapparaît, mais prochaine page ne compte pas comme un F5 car react=single page
 export function PopUpQuestion() {
-  const [isCollected, setIsCollected] = useState(false);
+  const [isOk, setIsOk] = useState(false);
 
-  const handleClickYes = () => {
-    setIsCollected(true);
-  };
+  useEffect(() => {
+    const storedChoice = localStorage.getItem('choice');
+    if (storedChoice === 'yes') {
+      setIsOk(true);
+    }
+  }, []);
 
-  const handleClickNo = () => {
-    setIsCollected(false);
-  };
+  function handleChoice(value) {
+    if (value === 'yes') {
+      setIsOk(true);
+      localStorage.setItem('choice', 'yes'); // Stocke "Oui" définitivement
+    } else {
+      setIsOk(true);
+      sessionStorage.setItem('choice', 'no'); // Stocke "Non" temporairement
+    }
+  }
+
+  ///////////////////////////////////////////////////////
 
   return (
-    <div className={!isCollected ? 'pop-up-question' : 'pop-up-question-no-display'}>
+    <div className={isOk ? 'pop-up-question-no-display' : 'pop-up-question'}>
       <h1>VOTRE EXPÉRIENCE, VOS CHOIX</h1>
       <p>
         Afin d’optimiser votre expérience, consentez-vous à l’enregistrement de vos articles favoris et de votre
@@ -22,11 +35,11 @@ export function PopUpQuestion() {
         ?
       </p>
 
-      <button className='button-yes' type='button' onClick={handleClickYes}>
+      <button className='button-yes' type='button' onClick={() => handleChoice('yes')}>
         Oui
       </button>
 
-      <button className='button-no' type='button' onClick={handleClickNo}>
+      <button className='button-no' type='button' onClick={() => handleChoice('no')}>
         Non
       </button>
     </div>
