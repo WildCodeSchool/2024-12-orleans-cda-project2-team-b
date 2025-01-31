cc04e756e9254f26c660451a2291141a8c1e67da
Chloe Seclier <seclier.chloe@gmail.com>
2025-01-31 10:30:46 +0100

    fix of the logic to show the pop up

--- src/components/pop-up-question.jsx
+++ src/components/pop-up-question.jsx
@@ -5,14 +5,12 @@ import './pop-up-question.scss';
 //Si l'utilisateur répond oui on garde en mémoire sa réponse en local, donc prochaine ouverture ou F5 la pop-up ne se réaffiche pas
 //Si il clique sur non alors au prochain F5 ou connexion pop-up réapparaît, mais prochaine page ne compte pas comme un F5 car react=single page
 export function PopUpQuestion() {
-  const [isOk, setIsOk] = useState(false);
-
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const storedChoice = localStorage.getItem('choice');
     if (storedChoice === 'yes') {
-      setIsOk(true);
+      setIsVisible(false);
     } else {
       setTimeout(() => {
         setIsVisible(true);
@@ -21,17 +19,14 @@ export function PopUpQuestion() {
   }, []);
 
   function handleChoice(value) {
+    setIsVisible(false);
     if (value === 'yes') {
-      setIsOk(true);
-      localStorage.setItem('choice', 'yes'); // Stocke "Oui" définitivement
-    } else {
-      setIsOk(true);
-      sessionStorage.setItem('choice', 'no'); // Stocke "Non" temporairement
+      localStorage.setItem('choice', 'yes');
     }
   }
 
   return (
-    <div className={isOk ? 'pop-up-question-no-display' : `pop-up-question ${isVisible ? 'show' : ''}`}>
+    <div className={!isVisible ? 'pop-up-question-no-display' : `pop-up-question ${isVisible ? 'show' : ''}`}>
       <h1>VOTRE EXPÉRIENCE, VOS CHOIX</h1>
       <p>
         Afin d’optimiser votre expérience, consentez-vous à l’enregistrement de vos articles favoris et de votre
