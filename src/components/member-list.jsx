a849fcb8dcebc440be247eae60835baa33c08782
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-03 12:23:55 +0100

    add pics and correctifs

--- src/components/member-list.jsx
+++ src/components/member-list.jsx
@@ -1,20 +1,20 @@
 export const memberList = [
   {
     name: 'Océane',
-    picture: '/images/template-picture.svg',
+    picture: '/images/oceane.jpg',
     github: 'https://github.com/ThisIsHowVillainsAreMade',
     linkedin: 'https://www.linkedin.com/in/oaiw/',
   },
   {
     name: 'Chloé',
-    picture: '/images/template-picture.svg',
+    picture: '/images/chloe.jpg',
     github: 'https://github.com/ChloeSeclier',
     linkedin: 'https://www.linkedin.com/in/chloé-seclier-b0a43a161/?originalSubdomain=fr',
   },
   {
     name: 'Mélissa',
-    picture: '/images/template-picture.svg',
-    github: 'https://github.com/ThisIsHowVillainsAreMade',
-    linkedin: 'https://www.linkedin.com/in/oaiw/',
+    picture: '/images/melissa.jpg',
+    github: 'https://github.com/alesbesdria',
+    linkedin: 'https://www.linkedin.com/in/m%C3%A9lissa-meirsman-8a2b492a9/',
   },
 ];
