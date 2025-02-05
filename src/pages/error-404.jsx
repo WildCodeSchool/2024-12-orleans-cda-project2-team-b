e5c1589e58a651b79f926213ee92cf3652d0aa55
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-05 16:55:34 +0100

    formation of content

--- src/pages/error-404.jsx
+++ src/pages/error-404.jsx
@@ -2,8 +2,11 @@ import './error-404.scss';
 
 export default function Error404() {
   return (
-    <>
-      <p>ERROR 404</p>
-    </>
+    <div className='page404'>
+      <div className='contentOf404'>
+        <img src='/images/404_Not_Found.svg' alt='Erreur 404' className='picture404' />
+        <img src='/icons/search.svg' alt='recherche' className='searchIconButton' />
+      </div>
+    </div>
   );
 }
