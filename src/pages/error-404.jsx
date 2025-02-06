3b0b5868de9d6e50334b17880668d2c69664b247
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-06 14:41:02 +0100

    change the path name and pic name turned to kebab case.*

--- src/pages/error-404.jsx
+++ src/pages/error-404.jsx
@@ -12,7 +12,7 @@ export default function Error404() {
   return (
     <div className='page404'>
       <div className='contentOf404'>
-        <img src='/images/404_Not_Found.svg' alt='Erreur 404' className='picture404' />
+        <img src='/images/404-not-found.svg' alt='Erreur 404' className='picture404' />
         <img
           src='/icons/search.svg'
           alt='recherche'
