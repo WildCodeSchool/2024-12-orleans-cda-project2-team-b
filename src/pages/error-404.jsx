f65f571d88a51e6389a50d08f115f3d44187fde4
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-06 12:31:14 +0100

    build of 404 page.

--- src/pages/error-404.jsx
+++ src/pages/error-404.jsx
@@ -1,11 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
 import './error-404.scss';
 
 export default function Error404() {
+  const navigate = useNavigate();
+
+  const searchRedirection = () => {
+    navigate('/recherche');
+  };
+
   return (
     <div className='page404'>
       <div className='contentOf404'>
         <img src='/images/404_Not_Found.svg' alt='Erreur 404' className='picture404' />
-        <img src='/icons/search.svg' alt='recherche' className='searchIconButton' />
+        <img
+          src='/icons/search.svg'
+          alt='recherche'
+          className='searchIconButton'
+          onClick={searchRedirection}
+          onKeyDown={(e) => e.key === 'Enter'}
+        />
       </div>
     </div>
   );
