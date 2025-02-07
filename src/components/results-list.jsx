761d1b0ed8c8bb1db542ff59aa647329ddf5b17f
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-07 14:54:41 +0100

    finish display list result

--- src/components/results-list.jsx
+++ src/components/results-list.jsx
@@ -7,15 +7,24 @@ export default function ResultsList({ article }) {
     <button className='article-button' title='Appuyer pour voir plus'>
       <div className='results-container'>
         <p>
-          {article.title && article.title.length <= 90
-            ? article.title
-            : article.title
-              ? article.title.slice(0, 90) + '...'
-              : 'Aucune description disponible'}
+          {(article.title &&
+            (window.innerWidth <= 700
+              ? article.title.length <= 30
+                ? article.title
+                : article.title.slice(0, 30) + '...'
+              : article.title.length <= 90
+                ? article.title
+                : article.title.slice(0, 90) + '...')) ||
+            'Aucun titre disponible'}
         </p>
 
         {/* penser à couper titre à40carc */}
-        {article.image_url && <img src={article.image_url} alt="photo de l'article" />}
+        {article.image_url ? (
+          article.image_url && <img src={article.image_url} alt="photo de l'article" />
+        ) : (
+          <img src='/no-image.svg' alt="photo de l'article" />
+        )}
+        {/* {article.image_url && <img src={article.image_url} alt="photo de l'article" />} */}
 
         <div className='under-image-results'>
           <div className='source'>{article.source_id}</div>
