72a20e6ebe6122da112078db2ab9fe25eea334c5
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-10 10:21:04 +0100

    delete inutile pictures

--- src/pages/display-favorite.jsx
+++ src/pages/display-favorite.jsx
@@ -1,14 +1,18 @@
 // import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+
+import { ChoicesContext } from '../contexts/choices-context';
 import DisplayArticle from './display-article';
 import './display-favorite.scss';
 
 export default function DisplayFavorite() {
+  const { articleTabl } = useContext(ChoicesContext);
   // const { id } = useParams();
   return (
     <>
       <div className='article-container'>
         <DisplayArticle />
-        <img className='background-article' src='/example-article.png' />
+        <img className='background-article' src={articleTabl.image_url} />
       </div>
     </>
   );
