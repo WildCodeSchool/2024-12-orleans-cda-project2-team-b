3a58b43ab1c2202623275446f5c463a0c90e63c3
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-14 10:36:50 +0100

    add the logic if no fav display noFav

--- src/pages/favourite.jsx
+++ src/pages/favourite.jsx
@@ -1,19 +1,20 @@
 import { useContext } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { ChoicesContext } from '../contexts/choices-context';
 import './favourite.scss';
+import NoFavourite from './no-favourite';
 
 export default function Favourite() {
   const articleId = 123; // Example dynamic ID (this could come from props, state, or API data)
   const { addArticleToFavourite, listFavorite } = useContext(ChoicesContext);
-
+  console.log(listFavorite.length);
   return (
     <>
       <p>Page de recherche des favoris</p>
 
       <Link to={`/favoris-article/${articleId}`}>Favori sélectionné</Link>
-      {listFavorite.length > 0 ? <p>j ai des favoris</p> : <Link to='/favoris-inconnu'>Erreur Pas de favoris</Link>}
+      {listFavorite.length > 0 ? <p>j ai des favoris</p> : <NoFavourite />}
     </>
   );
 }
