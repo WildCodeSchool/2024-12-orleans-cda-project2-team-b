23f85c690fd42a6ed5cb79e4eefdcc1949deb018
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-20 11:48:07 +0100

    logic in place, waiting the PR darkmode to merge it

--- src/components/like-button.jsx
+++ src/components/like-button.jsx
@@ -16,8 +16,13 @@ export default function LikeButton({ article }) {
     <>
       <button className='button-like' type='button' onClick={handleClickLike}>
         {(article?.article_id ? listFavourite.findIndex((fav) => fav.article_id === article.article_id) : -1) === -1 ? (
+          //darktheme =true use icons for dark
+          // useDarkTheme ?  <img src='/icons/like-for-dark-empty.svg' alt='not-like' title='Ajouter aux favoris' /> : <img src='/icons/like-empty.svg' alt='not-like' title='Ajouter aux favoris' />
+
+          // <img src={useDarkTheme? '/icons/like-for-dark-empty.svg':'/icons/like-empty.svg'} alt='not-like' title='Ajouter aux favoris' />
           <img src='/icons/like-empty.svg' alt='not-like' title='Ajouter aux favoris' />
         ) : (
+          //<img src={useDarkTheme? '/icons/like-for-dark-full.svg':'/icons/like-full.svg'} alt='like' title='Retirer des favoris' />
           <img src='/icons/like-full.svg' alt='like' title='Retirer des favoris' />
         )}
       </button>
