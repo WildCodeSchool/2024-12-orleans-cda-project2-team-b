fdcbc9bafd19bdfd8b305f88dba9ae2832af1864
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-20 15:41:42 +0100

    fix handleClick.

--- src/components/main-title.jsx
+++ src/components/main-title.jsx
@@ -10,13 +10,9 @@ export default function MainTitle() {
 
   return (
     <>
-      <div className='main-title-position'>
-        <h1 className='main-title-h1' onClick={handleClick}>
-          {"Wild's News"}
-        </h1>
-        <h2 className='main-title-h2' onClick={handleClick}>
-          {"L'actu tech"}
-        </h2>
+      <div className='main-title-position' onClick={handleClick}>
+        <h1 className='main-title-h1'>{"Wild's News"}</h1>
+        <h2 className='main-title-h2'>{"L'actu tech"}</h2>
       </div>
     </>
   );
