96bc7ab26d179f827c1b2db387fe5bcd22804bf3
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-20 15:14:44 +0100

    add text animation

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,14 @@ import './home.scss';
 export default function Home() {
   return (
     <>
-      <Spline scene='https://prod.spline.design/9A996GHdA8MXqhoX/scene.splinecode' />
+      <div className='container-animation'>
+        <div className='image-animation'>
+          <Spline scene='https://prod.spline.design/9A996GHdA8MXqhoX/scene.splinecode' />
+        </div>
+        <div className='text-animation'>
+          <Spline scene='https://prod.spline.design/cDZK06-Q-cNuygBZ/scene.splinecode' />{' '}
+        </div>
+      </div>
     </>
   );
 }
