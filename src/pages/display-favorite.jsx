665d847f66dea1ed5675f2fac291d2d7f8bacbe8
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-20 18:17:15 +0100

    open a branch to test

--- src/pages/display-favorite.jsx
+++ src/pages/display-favorite.jsx
@@ -1,18 +1,24 @@
-// import { useParams } from 'react-router-dom';
 import { useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 
+import { ButtonNav } from '../components/button-nav-article';
 import { ChoicesContext } from '../contexts/choices-context';
 import DisplayArticle from './display-article';
 import './display-favorite.scss';
 
 export default function DisplayFavorite() {
   const { articleChosen } = useContext(ChoicesContext);
+  const location = useLocation();
 
   return (
     <>
       <div className='article-container'>
         <DisplayArticle />
-        <img className='background-article' src={articleChosen.image_url} />
+        {location.pathname.includes('favoris') && <img className='background-article' src={articleChosen.image_url} />}
+      </div>
+      <div className='nav-article'>
+        <ButtonNav direction={-1} classIcon='icon-previous' texte='précédent' path={location.pathname} />
+        <ButtonNav direction={1} classIcon='icon-next' texte='suivant' path={location.pathname} />
       </div>
     </>
   );
