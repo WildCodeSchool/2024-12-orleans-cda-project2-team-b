0e454c53f61e9933c5ac5b645bd4bbec9b64f22f
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-20 16:03:33 +0100

    add image+title in 1 spline

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,13 +5,8 @@ import './home.scss';
 export default function Home() {
   return (
     <>
-      <div className='container-animation'>
-        <div className='image-animation'>
-          <Spline scene='https://prod.spline.design/9A996GHdA8MXqhoX/scene.splinecode' />
-        </div>
-        <div className='text-animation'>
-          <Spline scene='https://prod.spline.design/cDZK06-Q-cNuygBZ/scene.splinecode' />{' '}
-        </div>
+      <div className='animation'>
+        <Spline scene='https://prod.spline.design/9A996GHdA8MXqhoX/scene.splinecode' />
       </div>
     </>
   );
