8edcf2890c79695dd21b8bfec4330dfac3c71c15
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-20 15:11:18 +0100

    fix handleClick

--- src/components/main-title.jsx
+++ src/components/main-title.jsx
@@ -7,19 +7,14 @@ export default function MainTitle() {
   const handleClick = () => {
     navigate('/');
   };
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter' || event.key === ' ') {
-      navigate('/');
-    }
-  };
 
   return (
     <>
       <div className='main-title-position'>
-        <h1 className='main-title-h1' onClick={handleClick} onKeyDown={handleKeyDown}>
+        <h1 className='main-title-h1' onClick={handleClick}>
           {"Wild's News"}
         </h1>
-        <h2 className='main-title-h2' onClick={handleClick} onKeyDown={handleKeyDown}>
+        <h2 className='main-title-h2' onClick={handleClick}>
           {"L'actu tech"}
         </h2>
       </div>
