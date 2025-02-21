fbab1ed0ff08a1c3ff429e94b0a3b62cf1614695
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-21 15:26:54 +0100

    loader test

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import './app.scss';
 import BackgroundCard from './components/background-card';
 import Footer from './components/footer';
+import Loader from './components/loader';
 import MainTitle from './components/main-title';
 import Navbar from './components/navbar';
 import { PopUpQuestion } from './components/pop-up-question';
@@ -10,16 +12,28 @@ import { useDarkTheme } from './contexts/dark-theme-context';
 
 function App() {
   const { darkTheme } = useDarkTheme();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
 
   return (
-    <main className={`app-container ${darkTheme ? 'dark-mode' : ''}`}>
-      <Navbar />
-      <BackgroundCard />
-      <MainTitle />
-      <Outlet />
-      <PopUpQuestion />
-      <Footer />
-    </main>
+    <>
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <main className={`app-container ${darkTheme ? 'dark-mode' : ''}`}>
+          <Navbar />
+          <BackgroundCard />
+          <MainTitle />
+          <Outlet />
+          <PopUpQuestion />
+          <Footer />
+        </main>
+      )}
+    </>
   );
 }
 
