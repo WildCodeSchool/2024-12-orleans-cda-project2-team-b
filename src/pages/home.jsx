0e0a93701a6b7c8e66f26e5a6e7301e067ccdb6e
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-24 16:27:05 +0100

    no location !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,20 @@
 import Spline from '@splinetool/react-spline';
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 
 import Loader from '../components/loader';
 import './home.scss';
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
-  const location = useLocation();
-
   useEffect(() => {
-    if (location.pathname === '/') {
+    {
       setIsLoading(true);
       const timer = setTimeout(() => {
         setIsLoading(false);
       }, 1000);
       return () => clearTimeout(timer);
     }
-  }, [location]);
-
+  }, []);
   return (
     <>
       {isLoading && <Loader />}
