03fc42b1fcb9d6ba6b466b9d1f896c057ba45dab
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-02-24 15:08:50 +0100

    loader on home.jsx only

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import './app.scss';
 import BackgroundCard from './components/background-card';
 import Footer from './components/footer';
-import Loader from './components/loader';
 import MainTitle from './components/main-title';
 import Navbar from './components/navbar';
 import { PopUpQuestion } from './components/pop-up-question';
@@ -12,42 +10,17 @@ import { useDarkTheme } from './contexts/dark-theme-context';
 
 function App() {
   const { darkTheme } = useDarkTheme();
-  const [isLoading, setIsLoading] = useState(true);
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (location.pathname === '/') {
-      setIsLoading(true);
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 1000);
-      return () => clearTimeout(timer);
-    }
-  }, [location]);
-
-  const handleSearch = () => {
-    setIsLoading(true);
-
-    setTimeout(() => {
-      setIsLoading(false);
-      navigate('/');
-    }, 3000);
-  };
 
   return (
     <>
-      {isLoading && <Loader />}
-      {
-        <main className={`app-container ${darkTheme ? 'dark-mode' : ''}`}>
-          <Navbar onSearch={handleSearch} />
-          <BackgroundCard />
-          <MainTitle />
-          <Outlet />
-          <PopUpQuestion />
-          <Footer />
-        </main>
-      }
+      <main className={`app-container ${darkTheme ? 'dark-mode' : ''}`}>
+        <Navbar />
+        <BackgroundCard />
+        <MainTitle />
+        <Outlet />
+        <PopUpQuestion />
+        <Footer />
+      </main>
     </>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,37 @@
 import Spline from '@splinetool/react-spline';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
+import Loader from '../components/loader';
 import './home.scss';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setIsLoading(true);
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [location]);
+
   return (
     <>
-      <div className='animation-desktop'>
-        <Spline scene='https://prod.spline.design/9A996GHdA8MXqhoX/scene.splinecode' />
-      </div>
-      <div className='animation-mobile'>
-        <Spline scene='https://prod.spline.design/5LWIT65K-jrDiLgr/scene.splinecode' />
-      </div>
+      {isLoading && <Loader />}
+      {
+        <>
+          <div className='animation-desktop'>
+            <Spline scene='https://prod.spline.design/9A996GHdA8MXqhoX/scene.splinecode' />
+          </div>
+          <div className='animation-mobile'>
+            <Spline scene='https://prod.spline.design/5LWIT65K-jrDiLgr/scene.splinecode' />
+          </div>
+        </>
+      }
     </>
   );
 }
