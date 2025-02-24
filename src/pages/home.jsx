9cc72b313c69f800481f337b8688d346172fcf3e
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-24 11:30:33 +0100

    animation for mobile add

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,11 @@ import './home.scss';
 export default function Home() {
   return (
     <>
-      <div className='animation'>
+      <div className='animation-desktop'>
         <Spline scene='https://prod.spline.design/9A996GHdA8MXqhoX/scene.splinecode' />
+      </div>
+      <div className='animation-mobile'>
+        <Spline scene='https://prod.spline.design/5LWIT65K-jrDiLgr/scene.splinecode' />
       </div>
     </>
   );
